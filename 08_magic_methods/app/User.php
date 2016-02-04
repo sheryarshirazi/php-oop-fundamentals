5e5323a05348e432f807e8f4b8fa40c7c32843ee
Sheryar Shirazi <sheryarshirazi@gmail.com>
2016-02-05 00:08:14 +0500

    add extra comments

--- 08_magic_methods/app/User.php
+++ 08_magic_methods/app/User.php
@@ -1,41 +1,41 @@
 <?php
 class User {
-    
+
     /**
      * @var string
      */
     private $email;
-    
+
     /**
      * @var string
      */
     private $password;
-    
+
     /**
      * All properties that can be set directly
      * @var array
      */
     private $fillable = array('email', 'password');
-    
+
     /**
      * All properties that can be gotten directly
      * @var array
      */
     private $accessible = array('email', 'password');
-    
+
     /**
      * Class configuration only
      * @param array $params
      */
     public function __construct(Array $params = array()) {
-        
+
         if (count($params)) {
             foreach ($params as $key => $value) {
                 $this->$key = $value;
             }
         }
     }
-    
+
     /**
      * Directly set inaccessible but existing properties, if in $this->fillable array
      * @param string $name
@@ -43,48 +43,49 @@ class User {
      * @return void
      */
     public function __set ($name, $value) {
-        
+
         // Do not set if not fillable
         if (! in_array($name, $this->fillable)) {
             return false;
         }
-        
+        // Do not set the value if not exist
+        // other it will create it
         if (isset($this->$name)) {
             $this->$name = $value;
         }
     }
-    
+
     /**
      * Directly get inaccessible but existing properties, if in $this->accesible array
      * @param string $name
      * @return mixed
      */
     public function __get ($name) {
-        
+
         // Do not return if not accessible
         if (! in_array($name, $this->accessible)) {
             return NULL;
         }
-        
+
         return isset($this->$name) ? $this->$name : NULL;
     }
-    
+
     /**
      * Return accessible properties as a json object
      * @return string
      */
     public function __toString () {
-        
+
         $data = array();
-        
+
         // Only add property accessible
         foreach ($this->accessible as $key) {
             $data[$key] = $this->$key;
         }
-        
+
         return json_encode($data);
     }
-    
+
     /**
      * Log in a user
      * @return string
@@ -92,7 +93,7 @@ class User {
     public function login(){
         return 'Logging in with a vengeange ...';
     }
-    
+
     /**
      * Log in a user
      * @return string
@@ -100,5 +101,5 @@ class User {
      public function logout(){
         return 'Logging out ...';
     }
-    
+
 }
